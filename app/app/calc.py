9748afa0e81230a20fb977d33338149380454bcc
Oskar Kosobucki <43652764+Kosaaaaa@users.noreply.github.com>
2022-06-23 11:43:41 +0200

    start django project

---
+++ app/app/calc.py
@@ -0,0 +1,15 @@
+"""
+    Calculation functions created only for testing purpose
+"""
+
+NUMERIC = int | float
+
+
+def add(x: NUMERIC, y: NUMERIC) -> NUMERIC:
+    """Add y to x"""
+    return x + y
+
+
+def subtract(x: NUMERIC, y: NUMERIC) -> NUMERIC:
+    """Subtract y from x"""
+    return x - y
---
+++ app/app/tests.py
@@ -0,0 +1,36 @@
+from django.test import SimpleTestCase
+from app import calc
+
+
+class CalcTests(SimpleTestCase):
+    """Tests the calc module"""
+
+    def test_add_ints(self):
+        res = calc.add(5, 9)
+
+        self.assertEqual(res, 14)
+
+    def test_add_floats(self):
+        res = calc.add(5.5, 3.14)
+
+        self.assertAlmostEqual(res, 8.64)
+
+    def test_add_numbers(self):
+        res = calc.add(5, 3.14)
+
+        self.assertAlmostEqual(res, 8.14)
+
+    def test_subtract_ints(self):
+        res = calc.subtract(9, 3)
+
+        self.assertEqual(res, 6)
+
+    def test_subtract_floats(self):
+        res = calc.subtract(9.99, 3.1)
+
+        self.assertAlmostEqual(res, 6.89)
+
+    def test_subtract_numbers(self):
+        res = calc.subtract(9.99, 3)
+
+        self.assertAlmostEqual(res, 6.99)
