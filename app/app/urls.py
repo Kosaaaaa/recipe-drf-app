52bb83ecfcbb418717ab7a3040de9677fa8aa145
Oskar Kosobucki <43652764+Kosaaaaa@users.noreply.github.com>
2022-06-23 14:53:42 +0200

    prepare project for handling media and static files

--- app/app/urls.py
+++ app/app/urls.py
@@ -1,6 +1,8 @@
 """
 App url routes
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
@@ -19,3 +21,9 @@ urlpatterns = [
     path('api/user/', include('user.urls')),
     path('api/recipe/', include('recipe.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )
