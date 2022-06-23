1a0ab06babc59d402d2225e5081c6a61d3ef792d
Oskar Kosobucki <43652764+Kosaaaaa@users.noreply.github.com>
2022-06-23 13:22:36 +0200

    create Recipe API and test it

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -5,9 +5,12 @@ from django.urls import path, include
 
 from rest_framework.routers import DefaultRouter
 
+from recipe import views
+
 router = DefaultRouter()
 
 app_name = 'recipe'
+router.register('recipes', views.RecipeViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,3 +1,42 @@
 """
 Views for the recipe APIs
 """
+
+from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+from core.models import Recipe
+from recipe import serializers
+
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    """View for manage recipe APIs."""
+    serializer_class = serializers.RecipeDetailSerializer
+    queryset = Recipe.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    @staticmethod
+    def _params_to_ints(qs):
+        """Convert a list of strings to integers."""
+        return [int(str_id) for str_id in qs.split(',')]
+
+    def get_queryset(self):
+        """Retrieve recipes for authenticated user."""
+        queryset = self.queryset
+
+        return queryset.filter(
+            user=self.request.user
+        ).order_by('-id').distinct()
+
+    def get_serializer_class(self):
+        """Return the serializer class for request."""
+        if self.action == 'list':
+            return serializers.RecipeSerializer
+
+        return self.serializer_class
+
+    def perform_create(self, serializer):
+        """Create a new recipe."""
+        serializer.save(user=self.request.user)
