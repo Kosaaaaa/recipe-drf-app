417d9e0b00d382c96be76a3b67235afc906aff50
Oskar Kosobucki <43652764+Kosaaaaa@users.noreply.github.com>
2022-06-23 12:22:03 +0200

    add health-check and test it

--- app/app/urls.py
+++ app/app/urls.py
@@ -4,9 +4,11 @@ App url routes
 from django.contrib import admin
 from django.urls import path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from core import views as core_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/health-check/', core_views.health_check, name='health-check'),
     path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
     path(
         'api/docs/',
