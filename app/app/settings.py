458cdab0b495a4d51bcb4a422a0a09e6a821dbe8
Oskar Kosobucki <43652764+Kosaaaaa@users.noreply.github.com>
2022-07-02 14:00:14 +0200

    implement redis caching

--- app/app/settings.py
+++ app/app/settings.py
@@ -177,3 +177,10 @@ LOGGING = {
         'level': 'INFO',
     },
 }
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+        'LOCATION': f"redis://{os.environ.get('REDIS_HOST', 'redis')}:{os.environ.get('REDIS_PORT', '6379')}/"
+    }
+}
